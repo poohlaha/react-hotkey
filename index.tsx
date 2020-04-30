d7c3ba12c308d3b3de98e3bf43d47fbb114692d3
PhoenixSlade <ztyjr88@163.com>
2020-04-30 13:43:38 +0800

    添加index.tsx文件
    添加index.tsx文件

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
-import {HotkeysEvent} from '../keys/hotkey';
-import Hotkeys from './main';
+import {HotkeysEvent} from './keys/hotkey';
+import Hotkeys from './keys/hotkeys';
 
 export type OnKeyFun = (shortcut: string, evn: KeyboardEvent, handle: HotkeysEvent) => void;
 
 export interface IReactHotkeysProps {
-    keyName?: string;
+    keyName?: string | Array<any>;
     filter?: (event: KeyboardEvent) => boolean;
     onKeyUp?: OnKeyFun;
     onKeyDown?: OnKeyFun;
@@ -29,7 +29,7 @@ export default class ReactHotkeys extends React.Component<IReactHotkeysProps> {
     };
 
     static propTypes = {
-        keyName: PropTypes.string,
+        keyName: PropTypes.string || PropTypes.array,
         filter: PropTypes.func,
         onKeyDown: PropTypes.func,
         onKeyUp: PropTypes.func,
@@ -50,13 +50,13 @@ export default class ReactHotkeys extends React.Component<IReactHotkeysProps> {
         if (filter) {
             Hotkeys.filter = filter;
         }
-        Hotkeys.unbind(this.props.keyName as string);
-        Hotkeys.init(this.props.keyName as string, {splitKey}, this.onKeyDown);
+        Hotkeys.unbind(this.props.keyName);
+        Hotkeys.init(this.props.keyName, {splitKey}, this.onKeyDown);
         document && document.body.addEventListener('keyup', this.handleKeyUpEvent);
     }
 
     componentWillUnmount() {
-        Hotkeys.unbind(this.props.keyName as string);
+        Hotkeys.unbind(this.props.keyName);
         this.isKeyDown = true;
         this.handle = {} as HotkeysEvent;
         document && document.body.removeEventListener('keyup', this.handleKeyUpEvent);
@@ -78,7 +78,26 @@ export default class ReactHotkeys extends React.Component<IReactHotkeysProps> {
     handleKeyUpEvent(e: KeyboardEvent) {
         if (!this.isKeyDown) return;
         this.isKeyDown = false;
-        if (this.props.keyName && this.props.keyName.indexOf(this.handle.shortcut) < 0) return;
+        if (this.props.keyName && typeof this.props.keyName === 'string') { // string
+            if (this.props.keyName && this.props.keyName.indexOf(this.handle.shortcut) < 0) return;
+        } else if (this.props.keyName && Object.prototype.toString.call(this.props.keyName) === '[object Array]') { // array
+            let flag = true;
+            let arr: any = this.props.keyName || [];
+            arr.forEach((item: any) => {
+                let keys = item.keys || [];
+                if (keys && keys.length > 0) {
+                    keys.forEach((key: string) => {
+                        if (key.indexOf(this.handle.shortcut) !== -1) {
+                            flag = false;
+                            return false;
+                        }
+                    });
+                }
+            });
+
+            if (flag) return;
+        }
+
         this.onKeyUp(e, this.handle);
         this.handle = {} as HotkeysEvent;
     }
